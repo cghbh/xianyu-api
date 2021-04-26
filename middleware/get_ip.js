27b6c4700c97d67565f27ec7c070b2185fae4bc3
陈刚 <1670755868@qq.com>
2021-04-26 20:01:15 +0800

    ip矫正

--- middleware/get_ip.js
+++ middleware/get_ip.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 function getClientIP (ctx) {
   let ip= ctx.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
-    ctx.ip  ||
     ctx.connection.remoteAddress || // 判断 connection 的远程 IP
     ctx.socket.remoteAddress || // 判断后端的 socket 的 IP
     ctx.connection.socket.remoteAddress || ''
