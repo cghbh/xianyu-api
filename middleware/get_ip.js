e82c9c29b7c0d20454bca3883e2bf1a21390ab1c
陈刚 <1670755868@qq.com>
2021-04-26 19:55:31 +0800

    error modify

--- middleware/get_ip.js
+++ middleware/get_ip.js
@@ -1,9 +1,9 @@
 function getClientIP (ctx) {
   let ip= ctx.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
-    req.ip  ||
-    req.connection.remoteAddress || // 判断 connection 的远程 IP
-    req.socket.remoteAddress || // 判断后端的 socket 的 IP
-    req.connection.socket.remoteAddress || ''
+    ctx.ip  ||
+    ctx.connection.remoteAddress || // 判断 connection 的远程 IP
+    ctx.socket.remoteAddress || // 判断后端的 socket 的 IP
+    ctx.connection.socket.remoteAddress || ''
   if(ip) {
     ip = ip.replace('::ffff:', '')
   }
